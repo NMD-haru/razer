8b04e8d8f028434406f848de784a6d13ad7196b5
NMD-haru <111827134+NMD-haru@users.noreply.github.com>
2022-10-27 19:10:58 +0700

    main

---
+++ razer/razer/Data/razerContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using razer.Models;
+
+namespace razer.Data
+{
+    public class razerContext : DbContext
+    {
+        public razerContext (DbContextOptions<razerContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<razer.Models.Movie> Movie { get; set; } = default!;
+    }
+}
